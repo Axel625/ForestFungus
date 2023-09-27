80cab6dae71a129062ea16dbc333f2de9126ddfa
Axel625 <al222010625@gmail.com>
2023-09-27 13:12:00 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Product from './Components/Product';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Forest Fungus</h1>
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
