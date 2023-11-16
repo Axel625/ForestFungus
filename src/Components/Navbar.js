e6441a975ae1697e17f6f3a4c5fa947c5600e39b
Axel625 <al222010625@gmail.com>
2023-11-15 22:55:26 -0600

    Problema resuelto

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,23 +2,68 @@ import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from 'react-router-dom';
 import logo from '../Assets/Forest-preview.png';
+import Fab from '@mui/material/Fab';
+import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { useStateValue } from '../StateProvider';
+
+// Simulación de un contexto o estado compartido
+const useStateValue = () => {
+  return {
+    basket: [] 
+  };
+};
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
+
+const linkStyle = {
+  textDecoration: 'none',
+  color: 'white',
+};
+
+const fabStyle = {
+  backgroundColor: 'blue',
+  color: 'white',
+  width: '40px',
+  height: '40px',
+  marginLeft: '10px',
+};
+
+const navbarStyle = {
+  backgroundColor: '#ff7514',
+};
+
+const logoStyle = {
+  width: '100px',
+  marginRight: '10px',
+};
+
+const menuStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  flex: 1,
+};
 
 const Navbar = () => {
-  const [{ basket }] = useStateValue();
-  
-  const [anchorEl, setAnchorEl] = useState(null);
-
   const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { basket } = useStateValue();
 
   const handleSignIn = () => {
     setUserIsLoggedIn(true);
@@ -26,52 +71,63 @@ const Navbar = () => {
   };
 
   const handleSignOut = () => {
-  
     setUserIsLoggedIn(false);
     setAnchorEl(null);
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={navbarStyle}>
         <Toolbar>
-        
           <Link to="/">
-            <img src={logo} alt="Logo" style={{width: '100px'}} /> 
+            <img src={logo} alt="Logo" style={logoStyle} />
           </Link>
-        
+          <Box sx={menuStyle}>
+            <Link to="/Productos" style={linkStyle}>
+              <Button color="inherit" variant="outlined">
+                Products
+              </Button>
+            </Link>
+            <Link to="/IA" style={linkStyle}>
+              <Fab color="primary" aria-label="add" sx={fabStyle}>
+                <CatchingPokemonIcon />
+              </Fab>
+            </Link>
+          </Box>
+          
           <Menu
             anchorEl={anchorEl}
             open={Boolean(anchorEl)}
-            onClose={() => setAnchorEl(null)}  
+            onClose={() => setAnchorEl(null)}
           >
-
             {userIsLoggedIn ? (
-              <MenuItem onClick={handleSignOut} key="logout">
+              <MenuItem onClick={handleSignOut}>
                 <ExitToAppIcon /> Cerrar sesión
               </MenuItem>
             ) : (
-              <Link to="Inicio_de_Sesion" key="login">
+              <Link to="Inicio_de_Sesion">
                 <MenuItem onClick={handleSignIn}>
                   <AccountCircleIcon /> Iniciar sesión
                 </MenuItem>
               </Link>
             )}
-
           </Menu>
-
-          <Link to="/Checkout-page">
-            <IconButton>
-              <Badge badgeContent={basket?.length}>
+          
+          <Link to="/Checkout-page" style={linkStyle}>
+            <IconButton aria-label="cart" color="inherit" variant="outlined">
+              <StyledBadge badgeContent={basket?.length} color="error">
                 <ShoppingCartIcon />
-              </Badge>
+              </StyledBadge>
             </IconButton>
           </Link>
-        
-          <IconButton onClick={e => setAnchorEl(e.currentTarget)}>
+          
+          <IconButton
+            color="inherit"
+            variant="outlined"
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+          >
             <AccountCircleIcon />
           </IconButton>
-
         </Toolbar>
       </AppBar>
     </Box>
